5a6ad5c4f07b9cf99755d909f95f3404781e8666
1911481-Wang Yunhao <1595014015@qq.com>
2022-11-04 22:13:03 +0800

    配置多个代理

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import axios from "axios";
 
 class App extends Component {
   getStudentData = ()=>{
-    axios.get('http://localhost:3000/students').then(
+    axios.get('http://localhost:3000/api1/students').then(
+      response => {console.log('success', response.data);},
+      error => {console.log('fail', error)}
+    )
+  }
+  getCarData = ()=>{
+    axios.get('http://localhost:3000/api2/cars').then(
       response => {console.log('success', response.data);},
       error => {console.log('fail', error)}
     )
@@ -13,9 +19,11 @@ class App extends Component {
     return (
       <div>
         <button onClick={this.getStudentData}>get student data</button>
+        <button onClick={this.getCarData}>get car data</button>
       </div>
     );
   }
+
 }
 
 export default App;
