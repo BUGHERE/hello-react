3a86e693fd9615ec5d7010a42226a0940019ba52
1911481-Wang Yunhao <1595014015@qq.com>
2022-11-30 16:37:48 +0800

    antd基本使用

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,25 @@
-import React, {Component} from 'react';
-import {Route, Switch, Redirect} from 'react-router-dom'
-import About from './pages/About'
-import Home from './pages/Home'
-import Header from './components/Header'
-import MyNavLink from "./components/MyNavLink";
+import React, {Component} from 'react'
+import {Button, DatePicker, Space} from 'antd'
+import {WechatOutlined, SearchOutlined} from '@ant-design/icons'
 
 export default class App extends Component {
+
+
   render() {
     return (
       <div>
-        <Header/>
-        <div className="row">
-          <div className="col-xs-2 col-xs-offset-2">
-            <div className="list-group">
-              {/*原生（有刷跳转）*/}
-              {/*<a className="list-group-item" href="./about.html">About</a>*/}
-              {/*<a className="list-group-item active" href="./home.html">Home</a>*/}
-              <MyNavLink to="/about">About</MyNavLink>
-              <MyNavLink to="/home">Home</MyNavLink>
-            </div>
-          </div>
-          <div className="col-xs-6">
-            <div className="panel">
-              <div className="panel-body">
-                <Switch>
-                  {/*注册路由*/}
-                  <Route path="/about" component={About}/>
-                  <Route path="/home" component={Home}/>
-                  <Redirect to="/about"/>
-                </Switch>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Space direction="vertical">
+          <button>btn</button>
+          <Button type="primary">Primary Button</Button>
+          <Button>Primary Button</Button>
+          <WechatOutlined/>
+          <Button type="primary" icon={<SearchOutlined/>}>
+            Search
+          </Button>
+          <DatePicker/>
+        </Space>
       </div>
-    );
+    )
   }
 }
 
