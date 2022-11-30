81e791b9661e36819684f43effba2e5b52b95d83
1911481-Wang Yunhao <1595014015@qq.com>
2022-11-30 16:51:08 +0800

    antd自定义主题颜色

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
 import React, {Component} from 'react'
+import {ConfigProvider} from 'antd'
 import {Button, DatePicker, Space} from 'antd'
 import {WechatOutlined, SearchOutlined} from '@ant-design/icons'
 
+
 export default class App extends Component {
-
-
   render() {
     return (
-      <div>
-        <Space direction="vertical">
-          <button>btn</button>
-          <Button type="primary">Primary Button</Button>
-          <Button>Primary Button</Button>
-          <WechatOutlined/>
-          <Button type="primary" icon={<SearchOutlined/>}>
-            Search
-          </Button>
-          <DatePicker/>
-        </Space>
-      </div>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: '#00b96b'
+          }
+        }}
+      >
+        <div>
+          <Space direction="vertical">
+            <button>btn</button>
+            <Button type="primary">Primary Button</Button>
+            <Button>Primary Button</Button>
+            <WechatOutlined/>
+            <Button type="primary" icon={<SearchOutlined/>}>
+              Search
+            </Button>
+            <DatePicker/>
+          </Space>
+        </div>
+      </ConfigProvider>
     )
   }
 }
